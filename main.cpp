1b7a62f37b3fcb0a8ba5f34e12542155ccd5494f
T. Robert Ward <trironyard@icloud.com>
2016-10-02 18:49:27 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+int x = 3;
+    if ( x > 5 )
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
+        }
+
+    return 0;
+}
